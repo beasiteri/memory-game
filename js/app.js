675e53e835c2af9b9764d19b04ce746b31a57458
Bea <siteribea@gmail.com>
2019-05-29 17:59:41 +0200

    Add a function that checks if the cards match

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const card = document.querySelector('.deck');
+const array = [];
 
-// when the card is clicked check the target and call the openCard() function on that target
+/* when the card is clicked check the target and call the openCard() function on that target */
 card.addEventListener('click', function (event) {
     const target = event.target;
     if (target.nodeName === 'LI') {
@@ -9,7 +10,7 @@ card.addEventListener('click', function (event) {
     }
 });
 
-// open the card that was clicked - add or remove classes
+/* open the card that was clicked - add or remove classes */
 function openCard(event) {
     if (event.classList.contains('open', 'show') === false) {
         event.classList.add('open', 'show');
@@ -25,6 +26,40 @@ function pushArray(event) {
     } else if (array.length === 1) {
         array.push(event);
         checkMatch();
+    }
+}
+
+/* check if the two cards match. if not flip face down */
+function checkMatch() {
+    var first = array[0].classList;
+    var second = array[1].classList
+    if (array[0].children[0].classList[1] === array[1].children[0].classList[1]) {
+        first.add('match');
+        second.add('match');
+        first.add('animated', 'bounce');
+        second.add('animated', 'bounce');
+        array.length = 0;
+    } else {
+        /* first.add('animated', 'jello'); */
+        /* second.add('animated', 'jello'); */
+        setTimeout(function () {
+            first.remove('open', 'show');
+        }, 800);
+
+        setTimeout(function () {
+            second.remove('open', 'show');
+        }, 800);
+        // reset the length of array to zero 
+        array.length = 0;
+        // change the color of stars to red
+        star[moves].style.color = 'red';
+        moves++;
+    }
+    if (moves === 10) {
+        setTimeout(function () {
+            restartGame();
+            changeStarColor();
+        }, 700);
     }
 }
 
